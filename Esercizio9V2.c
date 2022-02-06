0e8ae97718e599630621e3020f44172a07a42ff3
Alessandro <cesaro.alessandro@iisviolamarchesini.edu.it>
2022-02-06 17:47:40 +0100

    Base Impostata

---
+++ Esercizio9V2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int numeroA, numeroB, numeroC, vero;
+
+    do
+    {
+        printf("Inserire i numeri in ordine strettamente crescente (A<B<C) e maggiori di 0 \n");
+        printf("Inserisci A \n");
+        scanf("%d", &numeroA);
+        printf("Inserisci B \n");
+        scanf("%d", &numeroB);
+        printf("Inserisci C \n");
+        scanf("%d", &numeroC);
+
+        if (numeroA < numeroB < numeroB)
+        {
+            vero = 1;
+        }
+        else
+        {
+            vero = 0;
+        }
+
+    }
+    while(numeroA < 0 || numeroB < 0 || numeroC < 0 || vero == 0);
+
+    printf("Il valore maggiore è %d \n", numeroC);
+    printf("Il valore minimo è %d \n", numeroA);
+    
+    return 0;
+}
